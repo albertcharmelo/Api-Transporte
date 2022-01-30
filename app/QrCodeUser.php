e47ba8338328c0e92d98b4a5ce7d93ecf848eb45
albertcharmelocontacto@gmail.com <albertcharmelocontacto@gmail.com>
2022-01-30 13:04:43 -0400

    relacion con usuario

--- app/QrCodeUser.php
+++ app/QrCodeUser.php
@@ -9,6 +9,9 @@ class QrCodeUser extends Model
     protected $table = 'users_qr';
     protected $fillable = ['users_id', 'qr_image','qr_idShow','qr_name'];
 
-
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id','id');
+    }
 
 }

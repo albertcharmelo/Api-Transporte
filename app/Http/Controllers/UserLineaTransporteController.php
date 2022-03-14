a30624835bbfb606fdd58b015fba287f1913026d
albertcharmelocontacto@gmail.com <albertcharmelocontacto@gmail.com>
2022-03-14 11:50:55 -0400

    Controlador Obtener linea de buses y su choferes

--- app/Http/Controllers/UserLineaTransporteController.php
+++ app/Http/Controllers/UserLineaTransporteController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\UserLineaTransporte;
 use Illuminate\Http\Request;
 
 class UserLineaTransporteController extends Controller
 {
     
-    
+    public function getBusLines(Request $request){
+        $buslines = UserLineaTransporte::all();
+        return response()->json([
+            'message'=>'Buses obtenidos exitosamente',
+            'buslines'=>$buslines,
+
+        ]);
+    }
+
+
+    public function getBusCollection(Request $request){
+        $buslines = UserLineaTransporte::where('id',$request->busLineId)->get()->first();
+        
+        return response()->json([
+            'message'=>'Buses obtenidos exitosamente',
+            'buslines'=>$buslines,
+        ]);
+    }
 
 
 }
